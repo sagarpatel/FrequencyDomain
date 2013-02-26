9133cab929e82a1c47765ed2fcd4b1218ab693ac
Sagar Patel <17.sagar.patel@gmail.com>
2013-02-26 18:15:09 -0500

    Parametrized the pseudo log scaling code,  --->
    can now tweak # of samples per decade in editor.

--- UnityFolder/Assets/Scripts/AudioDirectorScript.cs
+++ UnityFolder/Assets/Scripts/AudioDirectorScript.cs
@@ -10,6 +10,7 @@ public class AudioDirectorScript : MonoBehaviour
 	public AudioSource[] audioSourceArray = new AudioSource[maxTrackCount];
 
 	public float[] pseudoLogArray = new float[100];
+	public int[] samplesPerDecadeArray = new int[10];
 
 	// Use this for initialization
 	void Start () 
@@ -26,51 +27,29 @@ public class AudioDirectorScript : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		//audioSourceArray[0].GetSpectrumData(sampleArrayFreqBH, 0, FFTWindow.Blackman);
+		// get raw FFT data 
 		audioSourceArray[0].GetSpectrumData(sampleArrayFreqBH, 0, FFTWindow.BlackmanHarris);//Rectangular);
-		
-		/// first attempts at pseudo log array, FFT at max resolution
-		/// using pattern : 20+40+80+80+160+320+640+1280+2560+2560  = 7740
-		int logIndex = 0;
-		int loopCounter = 0;
-		//cleanup pseudolog array first
-		for(int i = 0; i < 100; i++)
+
+		// cleanup pseudolog array first
+		for(int i = 0; i < pseudoLogArray.Length; i++)
 			pseudoLogArray[i] = 0;
 		
-		for(int i = 0; i < 8000; i++)
+		// doing the pseudo log scale
+		int decadeIndex = 0;
+		int fftSampleCounter = 0;
+		for(int i = 0; i < pseudoLogArray.Length; i++)
 		{
-			if(i < 20)
-				loopCounter = 2;
-			else if(i < 60)
-				loopCounter = 4;
-			else if(i < 140)
-				loopCounter = 8;
-			else if(i < 220)
-				loopCounter = 8;
-			else if(i < 380)
-				loopCounter = 16;
-			else if(i < 700)
-				loopCounter = 32;
-			else if(i < 1340)
-				loopCounter = 64;
-			else if(i < 2620)
-				loopCounter = 128;
-			else if(i < 5180)
-				loopCounter = 256;
-			else if(i < 8000)
-				loopCounter = 256;
+			if( i != 0 && i%10 == 0)
+				decadeIndex++;
 
-			for(int j = 0; j < loopCounter; j++ )
+			for(int j = 0; j < samplesPerDecadeArray[decadeIndex]; j++ )
 			{
-				pseudoLogArray[logIndex] += sampleArrayFreqBH[i];
-				i++;
+				pseudoLogArray[i] += sampleArrayFreqBH[fftSampleCounter];
+				fftSampleCounter++;
 			}
-			logIndex++;			
 		}
 
 		SpeadLocalMaxima();
-
-	
 	}
 
 	void SpeadLocalMaxima()
