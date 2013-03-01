3514f4e37fb876ac4064071a428e0757a1c02368
Sagar Patel <17.sagar.patel@gmail.com>
2013-03-01 17:54:11 -0500

    Implemented basic velocity, gravity, friction
    Next is better player controls and camera that tilts with velocity.

--- UnityFolder/Assets/Scripts/PlayerScript.cs
+++ UnityFolder/Assets/Scripts/PlayerScript.cs
@@ -9,6 +9,10 @@ public class PlayerScript : MonoBehaviour
 	public float newHeight = 0;
 	public float oldHeight = 0;
 
+	public Vector3 velocity = new Vector3();
+	public float friction = 0.0f;
+	public float gravity = 0.0f;
+
 
 	MeshFieldGeneratorScript meshFieldGeneratorScript;
 
@@ -22,16 +26,40 @@ public class PlayerScript : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
+		Vector3 oldPosition = transform.position;
+
 		float xTranslation = Input.GetAxis("Horizontal") * hControlSpeed;
 		float yTranslation = Input.GetAxis("Vertical") * vControlSpeed;
-		transform.Translate( -yTranslation, 0 , xTranslation);
+		//transform.Translate( -yTranslation, 0 , xTranslation);
+		velocity += new Vector3( -yTranslation, 0 , xTranslation);
+		// only apply friction to translation, not gravity/height
+		velocity.x -= velocity.x * friction;
+		velocity.z -= velocity.z * friction;
+
 		
 		//Get New Height
+		oldHeight = newHeight;
 		newHeight = meshFieldGeneratorScript.getHeightFromPosition(transform.position.x, transform.position.z);
-	
-		Vector3 tempVec = transform.position;
-		tempVec.y = newHeight;
-		transform.position = tempVec;
+
+		// if ramping up
+		if( newHeight > oldHeight)
+		{
+			velocity.y += newHeight - oldHeight;
+		}
+
+		// if up in the air
+		if( oldPosition.y > newHeight)
+		{
+			velocity.y -= gravity ;
+		}
+		else 
+		{
+			// if not in the air, hug surface
+			velocity.y = 0;
+			oldPosition.y = newHeight;
+		}
+
+		transform.position = oldPosition + velocity * Time.deltaTime;
 	}
 
 
