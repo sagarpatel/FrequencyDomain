f458ba2fed24f6315824e722033a5388cba23167
Sagar Patel <17.sagar.patel@gmail.com>
2013-03-02 15:25:08 -0500

    Tweaking TES2, controls and camera

--- UnityFolder/Assets/Scripts/MainCameraScript.cs
+++ UnityFolder/Assets/Scripts/MainCameraScript.cs
@@ -4,23 +4,22 @@ using System.Collections;
 public class MainCameraScript : MonoBehaviour 
 {
 	GameObject meshFieldGenerator;
-	public float screenCenter = 0;
-	public int frequencyCount = 0;
+	public float distanceAhead = 0;
+	Vector3 cameraTarget = new Vector3();
 
 	// Use this for initialization
 	void Start () 
 	{
 		meshFieldGenerator = GameObject.Find("MainMeshField");
-		frequencyCount = ((MeshFieldGeneratorScript)meshFieldGenerator.GetComponent("MeshFieldGeneratorScript")).verticesFrequencyDepthCount;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		float frequencyScale = meshFieldGenerator.transform.localScale.z;
-		screenCenter = (frequencyScale * (float)frequencyCount)/2.0f;
+		cameraTarget = transform.parent.position;
+		cameraTarget.x -= distanceAhead;
 
-		transform.LookAt( new Vector3(0,-10,screenCenter), Vector3.up );
+		transform.LookAt( cameraTarget, Vector3.up );
 	}
 
 }
