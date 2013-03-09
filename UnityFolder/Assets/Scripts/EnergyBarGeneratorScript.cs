05cbf41ca97075ef3d316da28e110c8d02e579c0
Sagar Patel <17.sagar.patel@gmail.com>
2013-03-09 01:42:29 -0500

    Started implenting "environment", testing spiral

---
+++ UnityFolder/Assets/Scripts/EnergyBarGeneratorScript.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections;
+
+public class EnergyBarGeneratorScript : MonoBehaviour 
+{
+
+	public GameObject energyBarPiecePrefab;
+	public GameObject[] energyPiecesArray = new GameObject[10];
+
+	public float radius = 200.0f;
+	public float depthScale = 1.0f;
+	public float rotationCount = 1.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+		Vector3 tempPosition;
+		float rotationFactor = (rotationCount / (float)energyPiecesArray.Length) * (2*Mathf.PI) ;
+		for(int i =0; i < energyPiecesArray.Length; i++)
+		{
+			tempPosition = new Vector3( (float)i*depthScale , radius * Mathf.Cos( (float)i*rotationFactor), radius* Mathf.Sin( (float)i*rotationFactor)  );
+			energyPiecesArray[i] = (GameObject)Instantiate(energyBarPiecePrefab, tempPosition, transform.localRotation );
+			energyPiecesArray[i].transform.parent = transform;
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+
+
+}
