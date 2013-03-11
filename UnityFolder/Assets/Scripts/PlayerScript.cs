597010acad934fb7ecae7d84c59b36f62ba5b456
Sagar Patel <17.sagar.patel@gmail.com>
2013-03-11 16:15:10 -0400

    Implemented basic warp, linear +bkg color change
    Would like 3 stages with different boost factors

--- UnityFolder/Assets/Scripts/PlayerScript.cs
+++ UnityFolder/Assets/Scripts/PlayerScript.cs
@@ -14,12 +14,18 @@ public class PlayerScript : MonoBehaviour
 	public float rampUpFactor = 1.0f;
 	public float rampUpCounter = 0;
 
+	public float boostFactor = 1.0f;
+	public float energyCounter = 0;
+	public float originalFieldOfView = 90;
+
 	MeshFieldGeneratorScript meshFieldGeneratorScript;
+	Camera mainCamera;
 
 	// Use this for initialization
 	void Start () 
 	{
 		meshFieldGeneratorScript = (MeshFieldGeneratorScript)GameObject.Find("MainMeshField").GetComponent("MeshFieldGeneratorScript");
+		mainCamera = (Camera)GameObject.Find("Main Camera").GetComponent("Camera");
 	
 	}
 	
@@ -38,13 +44,10 @@ public class PlayerScript : MonoBehaviour
 		// apply new force to velocity
 		velocity += new Vector3( -yTranslation, 0 , xTranslation);
 
-
-
 		// only apply friction to translation, not gravity/height
 		velocity.x -= velocity.x * friction;
 		velocity.z -= velocity.z * friction;
 
-		
 		//Get New Height
 		newHeight = meshFieldGeneratorScript.getHeightFromPosition(transform.position.x -1, transform.position.z);
 		
@@ -70,8 +73,33 @@ public class PlayerScript : MonoBehaviour
 		}
 
 		// if oldPosition.y and newHeight are equal, oldPosition stays untouched.
-
 		transform.position = oldPosition + velocity * Time.deltaTime;
+
+		HandleBoost();
+	}
+
+	void HandleBoost()
+	{
+		if( Input.GetButton("Fire1") )
+		{
+			energyCounter += Time.deltaTime * boostFactor;
+		}
+		else
+		{
+			energyCounter -= Time.deltaTime * boostFactor;
+			if(energyCounter < 0)
+				energyCounter = 0;
+		}
+
+		float rgbValue =  energyCounter/(181 - originalFieldOfView) ;
+
+		if( originalFieldOfView + energyCounter < 180 )
+		{
+			mainCamera.fieldOfView = originalFieldOfView + energyCounter;
+			mainCamera.backgroundColor = new Color(rgbValue,rgbValue,rgbValue,rgbValue);
+		}
+
+
 	}
 
 
