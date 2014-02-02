ab0381ac73f36a65a6f0f4eeac5b3821d1efc3ba
Sagar Patel <17.sagar.patel@gmail.com>
2014-02-02 12:38:59 +0900

    Tested with old scene, just need to add tag to fix

---
+++ UnityFolder/Assets/Scripts/LiveAudioInputSelectorScript.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LiveAudioInputSelectorScript : MonoBehaviour 
+{
+	public string[] devicesArray;
+	public int currentlySelectedDeviceIndex = 0;
+
+	public bool isActive = false;
+	float textVerticalOffset = 100.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetButtonDown("Warp"))
+		{
+			if(currentlySelectedDeviceIndex >= devicesArray.Length -1)
+					currentlySelectedDeviceIndex = 0;
+				else
+					currentlySelectedDeviceIndex += 1;
+
+			// Call Audiodirector function with new device
+		}
+	
+	}
+
+
+	void OnGUI()
+	{
+
+
+		if(isActive)
+		{
+			devicesArray = Microphone.devices;
+
+	    	GUI.Label(new Rect(0,textVerticalOffset+0,200,20),"List of available devices:");
+	    	GUI.Label(new Rect(200,textVerticalOffset+0,200,20),"Tap the Warp button to switch devices");
+
+			float yPos = 0;
+	    	float offset = 10.0f;
+			foreach(string device in devicesArray)
+	    	{
+	    		GUI.Label(new Rect(10,textVerticalOffset+ 20 + yPos, 500, 20), device);
+	    		yPos += offset;
+	    	}
+
+			GUI.Label(new Rect(0,textVerticalOffset+ 40 + yPos,200,20),"Currently selected device:");
+			GUI.Label(new Rect(10,textVerticalOffset+ 60 + yPos,500,20), devicesArray[currentlySelectedDeviceIndex]);
+		}
+
+	}
+}
