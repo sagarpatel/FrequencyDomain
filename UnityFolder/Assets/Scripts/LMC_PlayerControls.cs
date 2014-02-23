afdae641f053bc5ed3aaa7eb4bc3b6a9ef96b330
Sagar Patel <17.sagar.patel@gmail.com>
2014-02-23 12:06:40 +0900

    Getting basic Leap input

---
+++ UnityFolder/Assets/Scripts/LMC_PlayerControls.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class LMC_PlayerControls : MonoBehaviour 
+{
+
+	PlayerScript playerScript;
+
+	Controller controller;
+
+	// Use this for initialization
+	void Start () 
+	{
+		playerScript = GameObject.FindWithTag("Player").GetComponent<PlayerScript>();
+		// Leap stuff
+		controller = new Controller();
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+		Frame currentFrame = controller.Frame();
+		Vector3 translationLMC = UnityVectorExtension.ToUnityScaled(currentFrame.Translation( controller.Frame(1) ) ) ;
+		Debug.Log( translationLMC );
+		transform.Translate(translationLMC, Space.Self);
+
+	}
+
+
+}
