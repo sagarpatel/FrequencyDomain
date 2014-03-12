74a72520104a3ea7afd1a79842e8a5dd3b668019
Sagar Patel <17.sagar.patel@gmail.com>
2014-03-12 19:37:40 +0900

    Implemented part lifetime and selfdestruct

---
+++ UnityFolder/Assets/Scripts/CreaturePart.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreaturePart : MonoBehaviour 
+{
+	float lifetime = 0;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		lifetime += Time.deltaTime;
+
+		if(lifetime > 6.0f)
+			Destroy(gameObject, 0.0f);
+	}
+}
