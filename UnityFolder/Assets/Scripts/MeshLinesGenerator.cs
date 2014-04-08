bfac36a6b2cb29571fd65748a4b7cfa7f7eeb58d
Sagar Patel <17.sagar.patel@gmail.com>
2014-04-08 17:28:18 +0900

    Tried usnig old normals trick, still weird errors

--- UnityFolder/Assets/Scripts/MeshLinesGenerator.cs
+++ UnityFolder/Assets/Scripts/MeshLinesGenerator.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -17,11 +17,25 @@ public class MeshLinesGenerator : MonoBehaviour
 
 	AudioDirectorScript audioDirector;
 
+	public int verticesFrequencyDepthCount = 200;
+	Mesh calculationsMiniMesh;
+	Vector3[] miniVertsArray;
+
+	Vector3[] vertsArrayLast2;
+	Vector3[] newLineNormals;
+
+	float xScale = 1.0f;
+	float zScale = 1.0f;
 
 	// Use this for initialization
 	void Start () 
 	{
 		audioDirector = (AudioDirectorScript) GameObject.FindWithTag("AudioDirector").GetComponent("AudioDirectorScript");
+		GenerateCalculationsMiniMesh();
+
+		vertsArrayLast2 = new Vector3[2 * verticesFrequencyDepthCount];
+		newLineNormals = new Vector3[verticesFrequencyDepthCount];
+
 	}
 	
 	// Update is called once per frame
@@ -105,8 +119,36 @@ public class MeshLinesGenerator : MonoBehaviour
 		mesh.vertices = verticesArray;
 		//mesh.triangles = trianglesList.ToArray();
 		//mesh.RecalculateNormals();
+
+
+
+
 		mesh.SetIndices (indicesArray, MeshTopology.Lines, 0);
+
+		// calculate normals
+
+		// push down normals
+		for(int i = 0; i < verticesFrequencyDepthCount; i ++)
+		{
+			vertsArrayLast2[i + verticesFrequencyDepthCount] = vertsArrayLast2[i];
+		}
+		// insert new data
+		for(int i = 0; i < verticesFrequencyDepthCount; i++)
+		{
+			vertsArrayLast2[i] = verticesArray[i];
+		}
 		
+		calculationsMiniMesh.vertices = vertsArrayLast2;
+		calculationsMiniMesh.RecalculateNormals();
+
+		for(int i = 0; i < verticesFrequencyDepthCount; i++)
+		{
+			newLineNormals[i] = calculationsMiniMesh.normals[i];
+		}
+
+		mesh.normals = newLineNormals;
+
+
 
 		//Debug.Log("MESH GENERATED");
 
@@ -115,8 +157,84 @@ public class MeshLinesGenerator : MonoBehaviour
 
 		newMeshLineGO.renderer.material = meshMaterial;
 		
-		newMeshLineGO.renderer.material.color = 3.0f * audioDirector.calculatedRGB;;
+		newMeshLineGO.renderer.material.color = 3.0f * audioDirector.calculatedRGB;
 	}
+
+
+	void GenerateCalculationsMiniMesh()
+	{
+
+		GameObject calculationsMiniMeshGameObject = new GameObject("CalculationsMiniMesh");
+		calculationsMiniMeshGameObject.AddComponent("MeshFilter");
+		//calculationsMiniMeshGameObject.AddComponent("MeshRenderer");
+		calculationsMiniMesh = calculationsMiniMeshGameObject.GetComponent<MeshFilter>().mesh;
+
+        List<int> trisList = new List<int>();
+        List<Vector3> vertsList = new List<Vector3>();
+
+        List<Vector2> tempUVList = new List<Vector2>();
+
+        // initial line
+        for(int j = 0; j < verticesFrequencyDepthCount; j++)
+    	{
+    		vertsList.Add( new Vector3(0,0,j * zScale) );
+    		tempUVList.Add( new Vector2(0,0) );
+    	}
+
+    	// populate the rest of the vertices, triangles
+    	// use verticesFrequencyDepthCount to shift between frewuency collumns
+
+    	// generate only one extra row for normals calculations
+        for(int i = 1; i < 2; i++)
+        {
+        	for(int j = 0; j < verticesFrequencyDepthCount; j += 2)
+        	{
+        		// bottom left triangle
+        		vertsList.Add( new Vector3(i * xScale,0, j * zScale) );
+        		int currentListIndex = vertsList.Count -1;
+
+	        	trisList.Add(currentListIndex);
+	        	trisList.Add(currentListIndex - verticesFrequencyDepthCount);
+	        	trisList.Add(currentListIndex - verticesFrequencyDepthCount + 1);
+
+	        	// fill triangles in between this and previous triangle below
+	        	if( j > 0) // is not at the edge
+	        	{
+	        		// bottom left triangle
+	        		trisList.Add(currentListIndex -1);
+		        	trisList.Add(currentListIndex - verticesFrequencyDepthCount -1);
+		        	trisList.Add(currentListIndex - verticesFrequencyDepthCount);
+
+		        	// top right triangle
+		        	trisList.Add(currentListIndex);
+		        	trisList.Add(currentListIndex - 1);
+		        	trisList.Add(currentListIndex - verticesFrequencyDepthCount);
+	        	}	
+
+	        	// top right triangle
+	        	vertsList.Add( new Vector3( i*xScale,0, (j + 1)*zScale ) );
+	        	currentListIndex++;
+	        	
+	        	trisList.Add(currentListIndex);
+	        	trisList.Add(currentListIndex - 1);
+	        	trisList.Add(currentListIndex - verticesFrequencyDepthCount);
+  	
+	        	tempUVList.Add( new Vector2(0,0) );
+	        	tempUVList.Add( new Vector2(0,0) );
+        	}
+        }
+
+		calculationsMiniMesh.Clear();
+		calculationsMiniMesh.MarkDynamic();
+		calculationsMiniMesh.vertices = vertsList.ToArray();
+		calculationsMiniMesh.uv = tempUVList.ToArray();
+		calculationsMiniMesh.triangles = trisList.ToArray();
+		calculationsMiniMesh.RecalculateNormals();
+
+		miniVertsArray = calculationsMiniMesh.normals;
+
+	}
+
 
 
 
