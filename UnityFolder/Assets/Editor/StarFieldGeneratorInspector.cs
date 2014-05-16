575a27adfd04b6a54bf3172ef5c24abda841960a
Sagar Patel <17.sagar.patel@gmail.com>
2014-05-16 09:10:36 +0900

    Starting work on moving mesh genrator, new scene, etc

---
+++ UnityFolder/Assets/Editor/StarFieldGeneratorInspector.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(StarFieldGenerator))]
+public class StarFieldGeneratorInspector : Editor 
+{
+
+	public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        
+        StarFieldGenerator myScript = (StarFieldGenerator)target;
+        if(GUILayout.Button("Generate Star Field"))
+        {
+            myScript.GenerateStarField();
+        }
+    }
+}
---
+++ UnityFolder/Assets/Scripts/StarFieldGenerator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarFieldGenerator : MonoBehaviour 
+{
+
+	public GameObject startModel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void GenerateStarField()
+	{
+		Debug.Log("Generating Star Field");
+	}
+}
