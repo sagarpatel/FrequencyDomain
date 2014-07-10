11366b55f11af94d565e15357d195963347e66d7
Sagar Patel <17.sagar.patel@gmail.com>
2014-07-10 19:42:27 +0900

    Testing out new camera lerp. Works, but need to move target as well

---
+++ UnityFolder/Assets/Scripts/CameraHolderLerp.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraHolderLerp : MonoBehaviour 
+{
+	public Transform cameraHolderTransform;
+	float lerpSpeed = 4.0f;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+		Vector3 targetPos = cameraHolderTransform.position;
+		Quaternion targetRot = cameraHolderTransform.rotation;
+
+		Vector3 finalPos = Vector3.Lerp(transform.position, targetPos, lerpSpeed * Time.deltaTime);
+		Quaternion finalRot = Quaternion.Slerp(transform.rotation, targetRot, lerpSpeed * Time.deltaTime);
+
+		transform.position = finalPos;
+		transform.rotation = finalRot;
+
+	}
+
+
+}
