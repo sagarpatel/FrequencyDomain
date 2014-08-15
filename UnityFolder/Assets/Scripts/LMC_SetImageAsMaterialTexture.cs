682a9f7674bbe8f4a4b94b351db0fba9469cddff
Sagar Patel <17.sagar.patel@gmail.com>
2014-08-15 19:55:56 +0900

    Starting Leap Camera image experiment on skybox

---
+++ UnityFolder/Assets/Scripts/LMC_SetImageAsMaterialTexture.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+
+public class LMC_SetImageAsMaterialTexture : MonoBehaviour 
+{
+	// experimenting with --> https://developer.leapmotion.com/documentation/skeletal/csharp/devguide/Leap_Images.html
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
