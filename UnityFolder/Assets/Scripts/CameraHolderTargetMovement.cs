5a1c06ff973c21d588ea359ff5e62903ad123017
Sagar Patel <17.sagar.patel@gmail.com>
2014-09-13 16:05:24 +0900

    Implemente hand depth based camera pos, works pretty well

--- UnityFolder/Assets/Scripts/CameraHolderTargetMovement.cs
+++ UnityFolder/Assets/Scripts/CameraHolderTargetMovement.cs
@@ -4,9 +4,11 @@ using System.Collections;
 public class CameraHolderTargetMovement : MonoBehaviour 
 {
 	Vector3 localPosMin = new Vector3(0,20,-100);
-	Vector3 localPosMax = new Vector3(0, 40, -300);
+	Vector3 localPosMax = new Vector3(0, 40, -500);
 
 	PVA creaturePVA;
+
+	public bool isOverwritten = false; // used to enable LMC hand depth to take over camera target pos setting
 
 	void Start () 
 	{
@@ -15,11 +17,20 @@ public class CameraHolderTargetMovement : MonoBehaviour
 	
 	void Update () 
 	{
+
+		if (isOverwritten == true)
+			return;
+
 		float velRatio = creaturePVA.velocity.magnitude / creaturePVA.maxVelocityMagnitude;
 		float angVelRatio = creaturePVA.rotationalVelocity.magnitude / creaturePVA.maxRotationalVelocityMagnitude;
 
 		float step = 0.5f * (velRatio + angVelRatio);
-		
-		transform.localPosition = Vector3.Lerp(localPosMin, localPosMax, step);
+
+		MoveCameraTargetPosition(localPosMin, localPosMax, step);
+	}
+
+	public void MoveCameraTargetPosition(Vector3 localMin, Vector3 localMax, float lerpStep)
+	{
+		transform.localPosition = Vector3.Lerp(localMin, localMax, lerpStep);
 	}
 }
---
+++ UnityFolder/Assets/Scripts/LMC_HandDepthCameraPos.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class LMC_HandDepthCameraPos : MonoBehaviour 
+{
+	Controller lmcController;
+
+	CameraHolderTargetMovement cameraHolderTagetMovement;
+	Vector3 localPosMin = new Vector3(0, 5, -20);
+	Vector3 localPosMax = new Vector3(0, 100, -1000);
+
+	void Start () 
+	{
+		lmcController = new Controller();
+		if (lmcController == null)
+			Debug.LogWarning("Cannot connect to controller. Make sure you have Leap Motion v2.0+ installed");
+
+		cameraHolderTagetMovement = GetComponent<CameraHolderTargetMovement>();
+	}
+	
+	void Update () 
+	{
+		cameraHolderTagetMovement.isOverwritten = false;
+		if (lmcController == null)
+			return;
+
+		cameraHolderTagetMovement.isOverwritten = true; // enforce lerp camera pos
+
+		Frame frame = lmcController.Frame();
+		HandList hands = frame.Hands;
+		Hand firstHand = hands[0];
+
+		Vector3 relativePalmPos = firstHand.PalmPosition.ToUnityScaled();
+		float handDepth = Mathf.Clamp( -relativePalmPos.z, -0.3f, 0.3f); // seems to range between -0.3 and 0.3
+		float normalizedStep = 0.5f + (0.5f * ( handDepth * 1.0f / 0.3f )); // this should range between 0 and 1
+
+		cameraHolderTagetMovement.MoveCameraTargetPosition(localPosMin, localPosMax, normalizedStep);
+	}
+}
