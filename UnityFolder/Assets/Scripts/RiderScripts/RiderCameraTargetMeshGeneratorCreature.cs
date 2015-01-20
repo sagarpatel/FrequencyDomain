846812d086be0500f1d87837410ae6c7a3433e39
Sagar Patel <17.sagar.patel@gmail.com>
2015-01-20 10:29:22 +0900

    Starting work on tracking mesh creature head (for camera follow when out of bounds). Not sample bounds test not working yet

---
+++ UnityFolder/Assets/Scripts/RiderScripts/RiderCameraTargetMeshGeneratorCreature.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RiderCameraTargetMeshGeneratorCreature : MonoBehaviour 
+{
+	public GameObject meshCreatureGeneratorHead;
+	public Camera riderCamera;
+	Plane[] riderCamPlanes;
+	Collider meshCreatureGeneratorHeadCollider;
+
+	void Start()
+	{
+		riderCamPlanes = GeometryUtility.CalculateFrustumPlanes(riderCamera);
+		meshCreatureGeneratorHeadCollider = meshCreatureGeneratorHead.GetComponent<Collider>();
+	}
+
+	void Update()
+	{
+		if(GeometryUtility.TestPlanesAABB(riderCamPlanes, meshCreatureGeneratorHeadCollider.bounds))
+		{
+
+		}
+		else
+		{
+			Debug.Log(" MESH HEAD OUTOF VIEW!");
+		}
+
+	}
+
+}
