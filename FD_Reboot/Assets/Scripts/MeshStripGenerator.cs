88d967ce148a8fd8edba2ecf6763b9a2580947e0
Sagar Patel <17.sagar.patel@gmail.com>
2015-06-07 15:45:10 +0900

    Setting up basic scripts for new mesh generation structure

---
+++ FD_Reboot/Assets/Scripts/MeshStripGenerator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeshStripGenerator : MonoBehaviour 
+{
+
+	public void GenerateMeshStrip(int collumnsCount, float collumnWidth)
+	{
+
+		GameObject meshStripGO = new GameObject();
+		meshStripGO.transform.parent = transform;
+		meshStripGO.transform.localPosition = Vector3.zero;
+		meshStripGO.transform.localRotation = Quaternion.identity;
+		meshStripGO.transform.localScale = Vector3.one;
+		meshStripGO.name = transform.name + "_MeshStrip";
+
+		meshStripGO.AddComponent<MeshRenderer>();
+
+
+	}
+
+}
