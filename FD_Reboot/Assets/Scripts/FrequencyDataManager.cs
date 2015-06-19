c7a88e51ec98f270e30778dd7693f0c3058fb4d8
Sagar Patel <17.sagar.patel@gmail.com>
2015-06-19 16:01:03 +0900

    Implemented basic frequency data processing, calibrated default values using sine sweep sound. Normal music looks a little too busy/messy at the moment

--- FD_Reboot/Assets/Scripts/FrequencyDataManager.cs
+++ FD_Reboot/Assets/Scripts/FrequencyDataManager.cs
@@ -3,11 +3,14 @@ using System.Collections;
 
 public class FrequencyDataManager : MonoBehaviour 
 {
-	const int m_rawFFTDataSize = 1024;
+	const int m_rawFFTDataSize = 8192;
 	float[] m_currentRawFFTDataArray;
 
-	const int m_processedFFTDataSize = 128;
+	const int m_processedFFTDataSize = 128; // separated into 8 sections
 	float[] m_processedFFTDataArray;
+
+	int[] samplesAccumulationArray = {2,4,5,8,10,20,41,200};
+	int samplesAccumulationStartIndexOffset = 10;
 
 	enum DataSources
 	{
@@ -29,10 +32,40 @@ public class FrequencyDataManager : MonoBehaviour
 
 	void ProcessRawFFTData()
 	{
-		// debug versoin
+		int samplesSum = 0 + samplesAccumulationStartIndexOffset;
+		int accumulationIndex = 0;
+		int accumulationInterval = m_processedFFTDataArray.Length/8;
+		for(int i = 0; i< m_processedFFTDataArray.Length; i++)
+		{
+			if(i % accumulationInterval == 0 && i != 0)
+				accumulationIndex += 1;
+
+			samplesSum += samplesAccumulationArray[accumulationIndex];
+		}
+		if(samplesSum > m_currentRawFFTDataArray.Length)
+			Debug.LogError("Sum of samples is too large: " + samplesSum + " , max is: " + m_currentRawFFTDataArray.Length);
+
+
+
+		accumulationIndex = 0;
+		accumulationInterval = m_processedFFTDataArray.Length/8;
+		float tempSum = 0;
+		int rawFFTIndex = 0;
+		int currentRawSamplesPerProcessedPoint = 0;
 		for(int i = 0; i < m_processedFFTDataArray.Length; i++)
 		{
-			m_processedFFTDataArray[i] = m_currentRawFFTDataArray[i];
+			if(i % accumulationInterval == 0 && i != 0)
+				accumulationIndex += 1;
+
+			tempSum = 0;
+			currentRawSamplesPerProcessedPoint = samplesAccumulationArray[accumulationIndex];
+			for(int j = 0; j < currentRawSamplesPerProcessedPoint; j++)
+			{
+				tempSum += m_currentRawFFTDataArray[rawFFTIndex + samplesAccumulationStartIndexOffset];
+				rawFFTIndex += 1;
+			}
+			m_processedFFTDataArray[i] = tempSum;///(float)currentRawSamplesPerProcessedPoint;
+			//Debug.Log("Sum for acuumuator: " + currentRawSamplesPerProcessedPoint + " , " + tempSum);
 		}
 	}
 
