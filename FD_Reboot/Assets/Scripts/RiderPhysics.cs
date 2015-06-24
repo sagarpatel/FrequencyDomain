2f5c8d5be8998b95a5deabae247bcbe0cc278873
Sagar Patel <17.sagar.patel@gmail.com>
2015-06-24 18:54:47 +0900

    Renamed PlayerPhysics to RiderPhysics

--- FD_Reboot/Assets/Scripts/RiderPhysics.cs
+++ FD_Reboot/Assets/Scripts/RiderPhysics.cs
@@ -1,50 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerPhysics : MonoBehaviour 
+public class RiderPhysics : MonoBehaviour 
 {
 	float currentHeight;
 	float m_currentProgressOnTerrain_Depth = 0; // 0 is front of terrain, 1 is the furthest back
 	float m_currentProgressOnTerrain_Widrg = 0; // -1 is full left, +1 is full right
-
+	
 	MeshTerrainGenerator m_meshTerrainGenerator;
-
+	
 	[Range(-1,1)]
 	public float d_widthTEst = 0.0f;
-
+	
 	[Range(0,1)]
 	public float d_DepthTest = 0.0f;
-
+	
 	void Start()
 	{
 		m_meshTerrainGenerator = FindObjectOfType<MeshTerrainGenerator>();
-
+		
 	}
-
+	
 	void Update()
 	{
-
+		
 		CalculateMinHeight(d_DepthTest,0);
-
+		
 	}
-
+	
 	void CalculateMinHeight(float terrainRatio_Depth, float terrainRatio_Width)
 	{
 		int meshStripsCount = m_meshTerrainGenerator.m_meshStripsPoolCount;
 		int frontStripIndex = m_meshTerrainGenerator.m_lastActivatedStripIndex;
-
+		
 		int targetMeshStripIndexOffset = (int)(terrainRatio_Depth * (float)meshStripsCount);
 		int targetMeshIndex = (frontStripIndex + targetMeshStripIndexOffset) % meshStripsCount;
 		MeshStripGenerator targetMeshStripGenerator = m_meshTerrainGenerator.m_meshStripGeneratorsArray[ targetMeshIndex ];
-
+		
 		Vector3 pos = Vector3.zero;
 		Quaternion rot = Quaternion.identity;
 		targetMeshStripGenerator.CalculatePositionOnStrip( d_widthTEst,0, out pos, out rot);
-
+		
 		transform.position = pos;
 		transform.rotation = rot;
 	}
-
-
-
 }
