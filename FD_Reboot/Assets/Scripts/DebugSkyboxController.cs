b379fc93c305881d9076a59a9a28aa0606a490e0
Sagar Patel <17.sagar.patel@gmail.com>
2015-07-03 18:47:38 +0900

    Added debug skybox/background color controller

---
+++ FD_Reboot/Assets/Scripts/DebugSkyboxController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugSkyboxController : MonoBehaviour 
+{
+	Skybox m_riderCameraSkybox;
+	Camera m_riderCamera;
+
+	void Start()
+	{
+		m_riderCameraSkybox = FindObjectOfType<Skybox>();
+		m_riderCamera = FindObjectOfType<Camera>();
+		SetToColor(Color.black);
+	}
+
+	void Update()
+	{
+
+		if(Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			SetToColor(Color.black);
+		}
+		else if (Input.GetKeyDown(KeyCode.Alpha2))
+		{
+			SetToColor(Color.white);
+		}
+		else if(Input.GetKeyDown(KeyCode.Alpha3))
+		{
+			SetToSkybox();
+		}
+
+	}
+
+	void SetToColor(Color bkgColor)
+	{
+		m_riderCameraSkybox.enabled = false;
+		m_riderCamera.backgroundColor = bkgColor;
+	}
+
+	void SetToSkybox()
+	{
+		m_riderCameraSkybox.enabled = true;
+	}
+
+
+}
