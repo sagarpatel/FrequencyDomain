3dbc9b2efbb86f26af840b2e2526e9cb781f3854
Sagar Patel <17.sagar.patel@gmail.com>
2015-07-07 20:16:42 +0900

    Added and implemented a few debug skyboxes (free from asset store)

--- FD_Reboot/Assets/Scripts/DebugSkyboxController.cs
+++ FD_Reboot/Assets/Scripts/DebugSkyboxController.cs
@@ -6,7 +6,7 @@ public class DebugSkyboxController : MonoBehaviour
 	Skybox m_riderCameraSkybox;
 	Camera m_riderCamera;
 
-	public Material m_normalSkybox;
+	public Material[] m_skyboxMaterialsArray;
 	public Material m_renderTextureSkybox;
 
 	void Start()
@@ -29,9 +29,33 @@ public class DebugSkyboxController : MonoBehaviour
 		}
 		else if(Input.GetKeyDown(KeyCode.Alpha3))
 		{
-			SetToSkybox_Normal();
+			SetToSkybox_Normal(0);
 		}
 		else if(Input.GetKeyDown(KeyCode.Alpha4))
+		{
+			SetToSkybox_Normal(1);
+		}
+		else if(Input.GetKeyDown(KeyCode.Alpha5))
+		{
+			SetToSkybox_Normal(2);
+		}
+		else if(Input.GetKeyDown(KeyCode.Alpha6))
+		{
+			SetToSkybox_Normal(3);
+		}
+		else if(Input.GetKeyDown(KeyCode.Alpha7))
+		{
+			SetToSkybox_Normal(4);
+		}
+		else if(Input.GetKeyDown(KeyCode.Alpha8))
+		{
+			SetToSkybox_Normal(5);
+		}
+		else if(Input.GetKeyDown(KeyCode.Alpha9))
+		{
+			SetToSkybox_Normal(6);
+		}
+		else if(Input.GetKeyDown(KeyCode.Alpha0))
 		{
 			SetToSkybox_RenderTexture();
 		}
@@ -43,10 +67,10 @@ public class DebugSkyboxController : MonoBehaviour
 		m_riderCamera.backgroundColor = bkgColor;
 	}
 
-	void SetToSkybox_Normal()
+	void SetToSkybox_Normal(int skyboxIndex)
 	{
 		m_riderCameraSkybox.enabled = true;
-		m_riderCameraSkybox.material = m_normalSkybox;
+		m_riderCameraSkybox.material = m_skyboxMaterialsArray[skyboxIndex];
 	}
 
 	void SetToSkybox_RenderTexture()
