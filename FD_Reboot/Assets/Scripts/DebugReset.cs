396a911f7d3b936cc7d6be3a2c6bca59ae404f85
Sagar Patel <17.sagar.patel@gmail.com>
2015-07-09 22:16:55 +0900

    Added debug reset key

---
+++ FD_Reboot/Assets/Scripts/DebugReset.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugReset : MonoBehaviour 
+{
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Escape) == true)
+		{
+			Debug.Log("Reset!");
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+
+}
