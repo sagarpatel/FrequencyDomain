13e8fdd6e3f6d9ddb3a80a44b352c39f2e6661af
Sagar Patel <17.sagar.patel@gmail.com>
2015-07-09 15:56:29 +0900

    Tons of ghost creature animation work, need to do raise and parts explode next

--- FD_Reboot/Assets/Scripts/GhostRiderCreature.cs
+++ FD_Reboot/Assets/Scripts/GhostRiderCreature.cs
@@ -47,6 +47,9 @@ public class GhostRiderCreature : MonoBehaviour
 	float m_ghostDepthRatio = 0;
 	float m_ghostDepthSpeed = 0.120f;
 	float m_ghostSpeedAccumulator = 0;
+	float m_bodyPartMoveStepScaler = 25.0f;
+	float m_moveToFrontAnimationDuration = 1.0f;
+	float m_headToTailDistanceTriggerThreashold = 1.0f;
 
 	public void InitializeGhostRiderCreature(MeshTerrainGenerator meshTerrainGenerator, Vector4[] movementsDataArray, Color[] colorDataArray, GameObject headPartPrefab, GameObject bodyPartPrefab, int bodyPartsCount)
 	{
@@ -64,9 +67,10 @@ public class GhostRiderCreature : MonoBehaviour
 		for(int i = 0; i < bodyPartsCount; i++)
 		{
 			GameObject bodyPart = (GameObject)Instantiate(bodyPartPrefab);
-			bodyPart.transform.parent = m_headPart.transform;
-			bodyPart.transform.localPosition = Vector3.zero;
-			bodyPart.transform.localRotation = Quaternion.identity;
+			bodyPart.name += "_" + i;
+			bodyPart.transform.parent = transform;
+			bodyPart.transform.position = m_headPart.transform.position;
+			bodyPart.transform.rotation = m_headPart.transform.rotation;
 
 			m_bodyPartsArray[i] = bodyPart;
 		}
@@ -100,15 +104,86 @@ public class GhostRiderCreature : MonoBehaviour
 
 			targetMeshStripGenerator.CalculatePositionOnStrip_Ghost( m_riderDataWidthRatio, m_riderDataHeightOffsetTerrain, out pos, out rot);
 
-			transform.position = pos;
-			transform.rotation = rot;
+			m_headPart.transform.position = pos;
+			m_headPart.transform.rotation = rot;
+			LerpBodyParts();
+
+			// first pos/rot etc set
+			if(m_moveCounter == 0)
+			{
+				for(int i = 1; i < m_bodyPartsArray.Length; i++)
+				{
+					m_bodyPartsArray[i].transform.position = m_headPart.transform.position;
+					m_bodyPartsArray[i].transform.rotation = m_headPart.transform.rotation;
+				}
+			}
 
 			m_moveCounter ++;
 			yield return null;
 		}
 
-		Destroy(gameObject);
+		Debug.Log(gameObject.name + " Main animation complete " + Time.frameCount);
+		
+		// main animation complete, now moving the crature to the front over fixed amount of time
+		m_moveCounter --;
+		float moveToFrontTimeCounter = 0;
+		while(moveToFrontTimeCounter < m_moveToFrontAnimationDuration)
+		{
+			meshStripsCount = m_meshTerrainGenerator.m_meshStripsPoolCount;
+			frontStripIndex = m_meshTerrainGenerator.m_lastActivatedStripIndex;	
 
+			float step = moveToFrontTimeCounter/m_moveToFrontAnimationDuration;
+			m_ghostDepthRatio = Mathf.Lerp( m_ghostDepthRatio, 1, step);
+			
+			targetMeshStripIndexOffset = (int)( m_ghostDepthRatio * (float)meshStripsCount);
+			targetMeshIndex = (frontStripIndex + targetMeshStripIndexOffset) % meshStripsCount;
+			targetMeshStripGenerator = m_meshTerrainGenerator.m_meshStripGeneratorsArray[ targetMeshIndex ];
+			
+			targetMeshStripGenerator.CalculatePositionOnStrip_Ghost( m_riderDataWidthRatio, m_riderDataHeightOffsetTerrain, out pos, out rot);
+			
+			m_headPart.transform.position = pos;
+			m_headPart.transform.rotation = rot;
+			LerpBodyParts();
+			
+			moveToFrontTimeCounter += Time.deltaTime;
+			yield return null;
+		}
+
+		Debug.Log(gameObject.name + "Move to front complete " + Time.frameCount);
+		
+		// head now at front position, waiting for body parts to catch up
+		Transform lastPartTransform = m_bodyPartsArray[m_bodyPartsArray.Length -1].transform;
+		while(true)
+		{
+			float lastPartToHeadDist = Vector3.Distance(m_headPart.transform.position, lastPartTransform.position);
+			if(lastPartToHeadDist < m_headToTailDistanceTriggerThreashold)
+			{
+				break;
+			}
+			else
+			{
+				LerpBodyParts();
+			}
+
+			yield return null;
+		}
+
+		Debug.Log(gameObject.name + " Parts catch up complete " + Time.frameCount);
+		
+		// do raise and explosion animation
+
+
+		Destroy(gameObject);
+	}
+
+	void LerpBodyParts()
+	{
+		m_bodyPartsArray[0].transform.position = Vector3.Lerp( m_bodyPartsArray[0].transform.position, m_headPart.transform.position, m_bodyPartMoveStepScaler * Time.deltaTime);
+		for(int i = 1; i < m_bodyPartsArray.Length; i++)
+		{
+			m_bodyPartsArray[i].transform.position = Vector3.Lerp( m_bodyPartsArray[i].transform.position, m_bodyPartsArray[i-1].transform.position, m_bodyPartMoveStepScaler * Time.deltaTime);
+			m_bodyPartsArray[i].transform.rotation = Quaternion.Slerp( m_bodyPartsArray[i].transform.rotation, m_bodyPartsArray[i-1].transform.rotation, m_bodyPartMoveStepScaler * Time.deltaTime);
+		}
 	}
 
 
