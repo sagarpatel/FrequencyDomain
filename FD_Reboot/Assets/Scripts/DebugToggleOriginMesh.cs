9a8ba22d5936588a134c3238080dbc0fe8126215
SagarPatel <17.sagar.patel@gmail.com>
2015-09-02 16:02:01 -0700

    Upgraded project to Unity 5.1.2 for stable OVR build. Added toggling of origin mesh

---
+++ FD_Reboot/Assets/Scripts/DebugToggleOriginMesh.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugToggleOriginMesh : MonoBehaviour
+{
+    private MeshStripGenerator m_originMeshStripGenerator;
+    private MeshTerrainGenerator m_meshTerrainGenerator;
+
+
+    void Start()
+    {
+        m_meshTerrainGenerator = FindObjectOfType<MeshTerrainGenerator>();
+        m_originMeshStripGenerator = m_meshTerrainGenerator.m_meshCreatureOriginMesh;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.B) == true)
+            m_originMeshStripGenerator.gameObject.SetActive(!m_originMeshStripGenerator.gameObject.activeSelf);
+
+    }
+
+}
